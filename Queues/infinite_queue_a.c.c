a0d553e4f577a5b39d959f576e71dde50519e6d6
Anna DeVries <31195314+h3r0toz3r0@users.noreply.github.com>
2021-03-04 09:22:50 -0500

    Update infinite_queue_a.c.c

--- Queues/infinite_queue_a.c.c
+++ Queues/infinite_queue_a.c.c
@@ -1,6 +1,9 @@
 /*    Infinite Queue C-Program
  *    Author: Anna DeVries
  *    Date: 2 Mar 2021
+ *
+ *    Useage: gcc -c rng.c
+ *            gcc -lm <file>.c rng.o -o <file> && ./<file>
  * 
  *    Description:  creates a infinite queue
  *                  tasks arrive with an exponential(2.0) distribution
